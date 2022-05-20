35ec412cc87cc359ec1a7da0b28890339b833986
DESKTOP-B1ARBUV\salra <duskwind@naver.com>
2022-05-20 17:53:27 +0900

    [UPDATE]컨트롤러 수정

--- hobbycampus/src/main/java/com/hobbycam/controller/mylessonContoller.java
+++ hobbycampus/src/main/java/com/hobbycam/controller/mylessonContoller.java
@@ -59,6 +59,7 @@ public class mylessonContoller {
 		}
 	
 		// add lessonlist
+		
 		/*
 		 * try {
 		 * 
@@ -73,8 +74,8 @@ public class mylessonContoller {
 		 * map.put("lessonExchangeState", lessonExchangeState);
 		 * 
 		 * String lessonTime = lessonRecordDao.getLessonTime(lessonScheduleIdx);
-		 * //System.out.println("data:"+lessonTime); mav.addObject("lessonTime",
-		 * lessonTime);
+		 * System.out.println("data:"+lessonTime);
+		 * mav.addObject("lessonTime",lessonTime);
 		 * 
 		 * System.out.println("성공"); } catch (Exception e) { e.printStackTrace();
 		 * System.out.println("오류"); }
@@ -87,7 +88,7 @@ public class mylessonContoller {
 		List<LessonRecordVO> lessonRecordList = lessonRecordDao.getLessonRecords(uIdx, lessonRecordState);
 		
 		mav.addObject("lessonRecordList", lessonRecordList);
-		mav.addObject("lessonRecordState", lessonRecordState);
+		mav.addObject("lessonRecordState",lessonRecordState);
 		mav.setViewName("/users/mylesson");
 
 		return mav;
